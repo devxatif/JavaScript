6519b080695f6584dfd5a2b91b8d5b3a9ffeff97
Md Atif Ansari <atifansaricse@gmail.com>
2026-01-14 15:11:04 +0000

    complete control_flow

---
+++ 05_iterations/three.js
@@ -0,0 +1,43 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+// for(const num of arr){
+//     console.log(num);
+//     console.log(typeof num);
+    
+// } // intresting style to print the array
+
+// const greeting = "Hello world"
+// for(const greet of greeting){
+//     console.log(`Each char is ${greet}`);
+//     console.log(typeof greet);
+    
+
+// }
+
+
+const map = new Map() //A Map is a special data structure used to store key–value pairs (just like objects), but it is more powerful and flexible than normal objects.
+
+map.set('IN', "India")
+map.set('USA', "United State of America")
+
+// console.log(map);
+
+// for(const [key, value] of map){
+//     console.log(key, value);
+    
+// }
+
+
+// const myObject = {
+//     game1: "NFS",
+//     game2: "spiderman"
+// }
+
+// for(const [key, value] of myObject){
+//     console.log(key, value);
+    
+// }// TypeError: myObject is not iterable
